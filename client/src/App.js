b70ee792570ea59e4e21d6dcaafcd8053293d209
lanetb <lanetb@dukes.jmu.edu>
2023-03-09 14:43:43 -0500

    started client side js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef, useEffect } from 'react';
+import Form from './components/UsernameForm';
+import Chat from './components/Chat';
+import io from 'socket.io-client';
+import immer from "immer"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
